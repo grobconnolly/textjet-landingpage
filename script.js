cc60e87cdfbd8406b100cdfe4742cf3005e61116
grobconnolly@gmail.com <grobconnolly@gmail.com>
2020-01-29 17:21:18 -0800

    mailchimp update

---
+++ script.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const config = require("./config.json");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url: "https://us20.api.mailchimp.com/3.0/lists/38d5502717",
+        method: "POST",
+        headers: {
+            "Authorization": "esther1 " + mykey
+        },
+        body: jsonData
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            res.sendFile(__dirname + "/failure.html");
+        } else {
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            }
+        }
+    });
+
+});
+
+var mykey = config.MY_KEY;
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000.");
+});
